439f13455c3f484c98300755e50d93950b307d49
Dial Cortez <dialnco@gmail.com>
2018-03-21 16:43:12 -0300

    new redirect

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = (req, res) => {
-  const SEND_URL = 'https://app.bookmein.xyz';
+  const SEND_URL = 'https://app.bkmein.com';
   const PATH = req.url;
   const Location = `${SEND_URL}${PATH}`;
   res.writeHead(301, { Location });
